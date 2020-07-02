d5e61926136193e37a4a6331f802020d6faf87f0
yolchunasib <yolchunasib@gmail.com>
2020-07-02 12:22:14 +0400

    Add project files.

---
+++ 020720/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace _020720
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo("az-AZ");
+
+            //DateTime now = DateTime.Now;
+
+            //DateTime holiday = new DateTime(2020, 09, 03);
+
+            //TimeSpan diff = holiday.Subtract(now);
+
+            //TimeSpan openHour = new TimeSpan(10, 0, 0);
+            //TimeSpan closeHour = new TimeSpan(18, 0, 0);
+
+            //if (openHour < now.TimeOfDay && closeHour > now.TimeOfDay)
+            //{
+            //    Console.WriteLine("Magaza aciqdi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Magaza baglidir");
+            //}
+
+            //Random random = new Random();
+
+            //double r = random.NextDouble();
+
+            //Console.WriteLine(r);
+
+            //uint a = uint.MinValue;
+
+            //Console.WriteLine(a);
+
+            //Console.WriteLine(char.IsLetterOrDigit('@'));
+
+            //string fullname = "Yolchu Nasib";
+            //double amount = 20;
+            //double balace = 43;
+
+            //string r = string.Format("Salam {0}, sizin hesabiniza {1} manat elave olundu. Balansiniz {2} azndir", fullname, amount, balace);
+
+            //Console.WriteLine(r);
+
+            //string city = "Baku";
+
+            //Console.WriteLine(city.Insert(1,"s"));
+            //Console.WriteLine(city.Trim());
+            //Console.WriteLine(city.Substring(1,2));
+
+            //Console.WriteLine(city.Contains("y"));
+            //Console.WriteLine(city.IndexOf("y"));
+
+            //city = city.Remove(3, 1);
+            //city = city.Insert(3, "ı");
+
+            //Console.WriteLine(city);
+
+            //string namesStr = "Saleh,Elvin,Kamil";
+
+            //string[] names = namesStr.Split(',');
+
+            //foreach (var item in names)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //int[] ages = { 21, 41, 23, 28, 32 };
+
+            //string agesStr = string.Join('@', ages);
+
+            //Console.WriteLine(agesStr);
+        }
+    }
+}
